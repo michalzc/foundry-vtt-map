6733920dc2528add05cb94fad0dc2c8812701631
Michał Zając <michal.t.zajac@gmail.com>
2022-08-03 00:03:05 +0200

    Partialy working, missing yaml processing.

--- project/DistPlugin.scala
+++ project/DistPlugin.scala
@@ -1,5 +1,6 @@
 import com.typesafe.sbt.less.Import.LessKeys.less
 import com.typesafe.sbt.web.Import.Assets
+import org.scalajs.linker.interface.Report
 import org.scalajs.sbtplugin.ScalaJSPlugin
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport.{fastLinkJS, scalaJSLinkerOutputDirectory}
 import sbt.Keys._
@@ -11,8 +12,14 @@ object DistPlugin extends AutoPlugin {
   override def trigger  = allRequirements
 
   object autoImport {
-    lazy val devDist       = taskKey[Seq[(File, File)]]("Prepare local version of system to include in FoundryVTT")
-    lazy val devDistOutput = settingKey[File]("Location for development dist output")
+    lazy val devDist                  = taskKey[Seq[(File, File)]]("Prepare local version of system to include in FoundryVTT")
+    lazy val devDistOutput            = settingKey[File]("Location for development dist output")
+    lazy val yamlSourceDirectory      = settingKey[File]("Source directory with YAML files")
+    lazy val systemYaml               = settingKey[File]("The main system file in YAML format")
+    lazy val distOutputDirectory      = settingKey[File]("Location for distribution output")
+    lazy val distBuildOptputDirectory = settingKey[File]("Location for distribution build directory")
+    lazy val distFile                 = settingKey[File]("Location for distribution file")
+    lazy val distSystemFile           = settingKey[File]("Location for distribution system file")
   }
 
   import autoImport._
@@ -21,67 +28,38 @@ object DistPlugin extends AutoPlugin {
 
     inConfig(Compile)(
       Seq(
-        devDist := copyFilesTask.value,
-        devDistOutput := target.value / moduleName.value
-      ))
+        devDist                  := buildTask(fastLinkJS, devDistOutput, "dev-dist").value,
+        devDistOutput            := target.value / s"${moduleName.value}-dev",
+        yamlSourceDirectory      := sourceDirectory.value / "yaml",
+        systemYaml               := yamlSourceDirectory.value / "system.yaml",
+        distOutputDirectory      := target.value / s"${moduleName.value}",
+        distBuildOptputDirectory := distOutputDirectory.value / "build",
+        distFile                 := distOutputDirectory.value / s"${moduleName.value}.zip",
+        distSystemFile           := distOutputDirectory.value / s"${moduleName.value}.json",
+      ),
+    )
   }
 
-  def copyFilesTask =
+  def buildTask(link: TaskKey[Attributed[Report]], outputDirectory: SettingKey[File], name: String) =
     Def.task {
-      val jsOutDir = (fastLinkJS / scalaJSLinkerOutputDirectory).value
-      val scripts = fastLinkJS.value.data.publicModules.toList.flatMap { module =>
+      val jsOutDir = (link / scalaJSLinkerOutputDirectory).value
+      val scripts = link.value.data.publicModules.toList.flatMap { module =>
         List(Option(jsOutDir / module.jsFileName), module.sourceMapName.map(jsOutDir / _))
       }.flatten
-      val lessOutDir = (Assets / less / resourceManaged).value
-      val files = (Assets / less).value ++ resources.value ++ scripts
-      val t = devDistOutput.value
-      val dirs = (lessOutDir +: jsOutDir +: resourceDirectories.value).toSet
-      val s = streams.value
-      val cacheStore = s.cacheStoreFactory make "dev-dist"
-      val flt: File => Option[File] = flat(t)
+      val lessOutDir                      = (Assets / less / resourceManaged).value
+      val files                           = (Assets / less).value ++ resources.value ++ scripts
+      val t                               = outputDirectory.value
+      val dirs                            = (lessOutDir +: jsOutDir +: resourceDirectories.value).toSet
+      val s                               = streams.value
+      val cacheStore                      = s.cacheStoreFactory make name
+      val flt: File => Option[File]       = flat(t)
       val transform: File => Option[File] = (f: File) => rebase(dirs, t)(f).orElse(flt(t))
       val mappings: Seq[(File, File)] = files.flatMap {
         case r if !dirs(r) => transform(r).map(r -> _)
-        case _ => None
+        case _             => None
       }
       s.log.debug("Copying scripts: " + mappings.mkString("\n\t", "\n\t", ""))
       Sync.sync(cacheStore)(mappings)
       mappings
     }
-
-//  def getJsModules(jsDir: File, jsLinkReport: Report): Seq[File] = {
-//    jsLinkReport.publicModules.toList.flatMap { module =>
-//      List(Option(jsDir / module.jsFileName), module.sourceMapName.map(mn => jsDir / mn))
-//    }.flatten
-//  }
-//
-//  def copyFiles(destDir: File, jsModuleFiles: Seq[File], resporceDir: File)(implicit logger: ManagedLogger): File = {
-//    mkDir(destDir)
-//
-////    val resDirCnt = Option(resporceDir.toPath.getNameCount)
-//    jsModuleFiles.foreach(copyToDir(_, destDir, None))
-////    (resporceDir ** ("*")).get().foreach(copyToDir(_, destDir, resDirCnt))
-//
-//    destDir
-//  }
-//
-//  def mkDir(dir: File)(implicit logger: ManagedLogger): File = {
-//    if(!dir.isDirectory && !dir.mkdirs()) throw new IllegalAccessException(s"Can't create ${dir}")
-//    dir
-//  }
-//
-//  def copyToDir(file: File, dir: File, basePathLen: Option[Int])(implicit logger: ManagedLogger): AnyVal = {
-//    val out = basePathLen.flatMap { cnt => //FIXME: it's terrible, there must be a better way
-//      val path = file.getParentFile.toPath
-//      val nameCount = path.getNameCount
-//      if(nameCount > cnt) {
-//        Option(path.subpath(cnt, nameCount))
-//      } else {
-//        None
-//      }
-//    }.map(dir / _.toString / file.getName).getOrElse( dir / file.getName)
-//
-//    if(file.isFile) IO.copyFile(file, out)
-//    else ()
-//  }
 }
